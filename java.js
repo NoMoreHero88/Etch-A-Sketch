82d3be64c923c88492fad3bdd5c283c2b3bb8de1
Mark Keenan <marcokeeman@gmail.com>
2023-04-12 18:45:43 +0900

    Add Black Gradient color option
    Can now paint using a color that increments the tile in a black with
    opacity that increases by 10% each time the tile is touched

--- java.js
+++ java.js
@@ -7,6 +7,7 @@ const rainbowBtn = document.querySelector('#rainbow');
 let paintColor = 'black';
 const eraserBtn = document.querySelector('#eraser');
 const buttons = document.querySelectorAll('.toggleBtn');
+const gradientBtn = document.querySelector('#gradient');
 
 function createDivs(width){
     removeAllChildNodes(container);
@@ -33,10 +34,20 @@ function paintClick(e){
     if(paintColor == 'rainbow'){
         let randomColor=Math.floor(Math.random()*16777215).toString(16);
         e.target.style.backgroundColor = "#"+randomColor;
+        e.target.style.opacity = 1;
     } else if(paintColor == 'blank'){
         e.target.style.backgroundColor = '';
-    } else {
+    } else if(paintColor == 'gradient'){
+        if(e.target.style.backgroundColor !== 'black'){
+            e.target.style.backgroundColor = 'black';
+            e.target.style.opacity = .1;
+        }else {        
+            let currentOpacity = e.target.style.opacity;
+            e.target.style.opacity = parseFloat(currentOpacity) + .1;
+        }
+    }else {
         e.target.style.backgroundColor = 'black';
+        e.target.style.opacity = 1;
     }
 }
 
@@ -63,6 +74,16 @@ eraserBtn.addEventListener('click', () => {
     if (paintColor !== 'blank'){
         eraserBtn.style.backgroundColor = 'skyblue';
         paintColor = 'blank';
+    } else {
+        paintColor = 'black';
+    }
+})
+
+gradientBtn.addEventListener('click', () => {
+    btnBackgroundOff();
+    if (paintColor !== 'gradient'){
+        gradientBtn.style.backgroundColor = 'skyblue';
+        paintColor = 'gradient';
     } else {
         paintColor = 'black';
     }
